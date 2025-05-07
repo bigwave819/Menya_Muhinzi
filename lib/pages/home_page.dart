6705c641542f856871087b638200e38710c6f715
Hirwa Tresor <waveb6133@gmail.com>
2025-05-07 13:44:04 +0200

    flexed the drawer

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -14,19 +14,75 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
   final pages = [
-      const ExplorePage(),
-      const ServicesPage(),
-      const CartPage(),
-      const ProfilePage(),
-    ];
-    int currentPage = 0;
-    
+    const ExplorePage(),
+    const ServicesPage(),
+    const CartPage(),
+    const ProfilePage(),
+  ];
+  int currentPage = 0;
+
+  void _changePage(int index) {
+    setState(() {
+      currentPage = index;
+    });
+    Navigator.pop(context); // close the drawer after selecting
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: const Drawer(),
+      drawer: Drawer(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            // Top part of Drawer
+            Column(
+              children: [
+                const DrawerHeader(
+                  child: CircleAvatar(
+                    radius: 70,
+                    foregroundImage: AssetImage("images/profile.jpg"),
+                  ),
+                ),
+                const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 25),
+                  child: Divider(color: Colors.grey),
+                ),
+                ListTile(
+                  leading: const Icon(IconlyBroken.home),
+                  title: const Text("Home"),
+                  onTap: () => _changePage(0),
+                ),
+                ListTile(
+                  leading: const Icon(IconlyBroken.call),
+                  title: const Text("Services"),
+                  onTap: () => _changePage(1),
+                ),
+                ListTile(
+                  leading: const Icon(IconlyBroken.buy),
+                  title: const Text("Cart"),
+                  onTap: () => _changePage(2),
+                ),
+                ListTile(
+                  leading: const Icon(IconlyLight.profile),
+                  title: const Text("Profile"),
+                  onTap: () => _changePage(3),
+                ),
+              ],
+            ),
+            // Bottom part of Drawer (Logout)
+            const Padding(
+              padding: EdgeInsets.only(left: 25),
+              child: ListTile(
+                leading: Icon(IconlyLight.logout),
+                title: Text("Logout"),
+              ),
+            ),
+          ],
+        ),
+      ),
+
       appBar: AppBar(
         title: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
@@ -40,23 +96,25 @@ class _HomePageState extends State<HomePage> {
         ),
         actions: [
           Padding(
-            padding: EdgeInsets.only(right: 8.0),
+            padding: const EdgeInsets.only(right: 8.0),
             child: IconButton.filledTonal(
               onPressed: () {},
               icon: badges.Badge(
-                badgeContent: Text(
+                badgeContent: const Text(
                   '3',
                   style: TextStyle(color: Colors.white, fontSize: 18),
                 ),
-                badgeStyle: badges.BadgeStyle(badgeColor: Colors.green),
+                badgeStyle: const badges.BadgeStyle(badgeColor: Colors.green),
                 position: badges.BadgePosition.topEnd(top: -15, end: -12),
-                child: Icon(IconlyBroken.notification),
+                child: const Icon(IconlyBroken.notification),
               ),
             ),
           ),
         ],
       ),
+
       body: pages[currentPage],
+
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: currentPage,
         onTap: (index) {
@@ -65,25 +123,25 @@ class _HomePageState extends State<HomePage> {
           });
         },
         type: BottomNavigationBarType.fixed,
-        items: [
+        items: const [
           BottomNavigationBarItem(
             icon: Icon(IconlyLight.home),
-            activeIcon: Icon(IconlyBold.home),
+            activeIcon: Icon(IconlyBroken.home),
             label: "Home",
           ),
           BottomNavigationBarItem(
             icon: Icon(IconlyLight.call),
-            activeIcon: Icon(IconlyBold.call),
+            activeIcon: Icon(IconlyBroken.call),
             label: "Services",
           ),
           BottomNavigationBarItem(
             icon: Icon(IconlyLight.buy),
-            activeIcon: Icon(IconlyBold.buy),
+            activeIcon: Icon(IconlyBroken.buy),
             label: "Cart",
           ),
           BottomNavigationBarItem(
             icon: Icon(IconlyLight.profile),
-            activeIcon: Icon(IconlyBold.profile),
+            activeIcon: Icon(IconlyBroken.profile),
             label: "Profile",
           ),
         ],
